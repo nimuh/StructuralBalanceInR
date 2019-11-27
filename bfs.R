add8405db2c1fcf9f28753f9cefe8e4e7be40cd9
Nima Azbijari <nazbijar@ucsd.edu>
2019-11-27 10:53:16 -1000

    super node creation is cpp, testing in bfs.R

--- bfs.R
+++ bfs.R
@@ -1,119 +1,39 @@
 
 library(igraph)
 library(queue)
+library(Rcpp)
+sourceCpp('superNodes.cpp')
 
-get_edge_sign_ht <- function(g) {
-  df <- get.data.frame(g, what="edges")
-  ht <- new.env(hash=TRUE)
-  for (i in 1:nrow(df)) {
-    from <- df[i, "from"]
-    to <- df[i, "to"]
-    sign <- df[i, "sign"]
-    ename1 <- paste("edge", from, to, sep="")
-    ename2 <- paste("edge", to, from, sep="")
-    ht[[ename1]] <- sign
-    ht[[ename2]] <- sign
-  }
-  ht
-}
-
-# grab adjacents of v in graph g
-get_adj <- function(queue, v, g, nodes) {
-  n <- neighbors(g, v)
-  for (i in 1:length(n)) {
-    e <- c(as.numeric(v),as.numeric(n[i]))
-    enqueue(queue, e)
-  }
-}
-
-
-
-positive_vibes_only <- function(g) {
-  
-  edge_signs <- get_edge_sign_ht(g)
-  df_edges <- get.data.frame(g, what='edges')
-  df_nodes <- get.data.frame(g, what='vertices')
-  df_nodes$visited <- FALSE
-  df_nodes$super <- 0
-  q <- create_queue("edges")
-  
-  # set super node number to 1
-  super_nu <- 1
-  
-  # still need to iterate on each node but check if it is visited
-  # after each time the queue is emptied out the super_nu value is incremented
-  # for the next super node
-  for (i in 1:nrow(df_nodes)) {
-    
-    if (df_nodes[i, 'visited'] == FALSE) {
-      
-      #curr <- df_edges[i, 'from']
-      curr <- i
-      df_nodes[i, "visited"] <- TRUE
-      df_nodes[i, "super"] <- super_nu
-      get_adj(q, curr, g, df_nodes)
-      
-      while (!is_empty(q)) {
-        e <- dequeue(q)
-        ename <- paste("edge", e[1], e[2], sep="")
-
-        if (!is.null(edge_signs[[ename]])) {
-          
-          # if neighbor positive and have not visited yet
-          if (df_nodes[e[2], "visited"] == FALSE & edge_signs[[ename]] == 1) {
-            
-            # assign current super node number
-            df_nodes[e[2], "super"] <- super_nu
-            df_nodes[e[2], "visited"] <- TRUE
-            get_adj(q, e[2], g, df_nodes)
-            
-          }
-          # if neighbor negative and in the same cluster
-          else if (edge_signs[[ename]] == -1 & df_nodes[e[2], "visited"] == TRUE) {
-            
-            if (df_nodes[e[2], 'super'] == df_nodes[e[1], 'super']) {
-              # remove from cluster
-              df_nodes[e[2], 'super'] <- 0
-              df_nodes[e[2], 'visited'] <- FALSE
-            }
-          }
-          
-        }
-        
-      }
-      
-      # increment after reaching all nodes from starting point
-      super_nu <- super_nu + 1
-      
-    }
-  }
-  
-  delete_queue(q)
-  df_nodes
-  
-}
 
 # TEST GRAPHS
+test <- function() {
+  sourceCpp('superNodes.cpp')
+  g1 <- graph_from_literal(1-2, 1-3, 2-3)
+  V(g1)$color <- 1
+  V(g1)$super <- -1
+  E(g1)$sign <- c(1, 1, 1)
+  g1_v <- get.data.frame(g1, what='vertices')
+  g1_e <- get.data.frame(g1, what='edges')
+  
+  g2 <- graph_from_literal(1-2, 1-3, 2-3)
+  V(g2)$color <- 1
+  V(g2)$super <- -1
+  E(g2)$sign <- c(1, 1, -1)
+  g2_v <- get.data.frame(g2, what='vertices')
+  g2_e <- get.data.frame(g2, what='edges')
+  
+  g3 <- graph_from_literal(1-2, 1-3, 2-3, 3-4)
+  V(g3)$color <- 1
+  V(g3)$super <- -1
+  E(g3)$sign <- c(1, 1, -1, 1)
+  g3_v <- get.data.frame(g3, what='vertices')
+  g3_e <- get.data.frame(g3, what='edges')
+  
+  create_super_nodes(g3_v, g3_e)
+  
+}
 
-# 1 supernode
-g1 <- graph_from_literal(1-2, 1-3, 2-3)
-E(g1)$sign <- c(1, 1, 1)
-positive_vibes_only(g1)
 
-# 2 super nodes
-g2 <- graph_from_literal(1-2, 1-3, 2-3)
-E(g2)$sign <- c(1, 1, -1)
-positive_vibes_only(g2)
 
-# 2 super nodes
-g3 <- graph_from_literal(1-2, 1-3, 2-3, 3-4)
-E(g3)$sign <- c(1, 1, -1, 1)
-positive_vibes_only(g3)
-
-# bipartite graph (this is weird, I don't know why it gives these supers)
-g4 <- graph_from_literal(1-4, 1-5, 1-6, 2-4, 2-5, 2-6, 3-4, 3-5, 3-6)
-E(g4)$sign <- c(1, 0, 1, 1, 0, 1, 1, 0, 1)
-get.data.frame(g4)
-positive_vibes_only(g4)
 
 
---
+++ superNodes.cpp
@@ -0,0 +1,109 @@
+#include <Rcpp.h>
+#include <queue>
+
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+DataFrame create_super_nodes(DataFrame g_v, DataFrame g_e) {
+
+    std::string node("node");
+    std::map<std::string, std::string> supers;
+    std::map<std::string, std::string> colors;
+    std::map<std::string, std::string> signs;
+    std::map< std::string, std::vector<std::string> > neigh;
+    std::queue<std::string> nias;
+
+    // THIS SECTION INITIATES ALL TABLES NEEDED
+    //////////////////////////////////////////////////////////
+    // THESE ARE ALL REFERENCES
+    // Color assignments 
+    CharacterVector color = g_v["color"];
+
+    // Super node assignments
+    CharacterVector super = g_v["super"];
+
+    // Node names
+    CharacterVector names = g_v["name"];
+
+    // Edges with signs
+    CharacterVector from = g_e["from"];
+    CharacterVector to = g_e["to"];
+    CharacterVector sign = g_e["sign"];
+
+    // use std:: string to convert vector elements to append them
+    for (int i = 0; i < names.size(); i++) {
+        std::string name = (node + std::string(names[i]));
+        supers[name] = super[i];
+        colors[name] = color[i];
+        nias.push(name);
+    }
+
+    for (int i = 0; i < from.size(); i++) {
+        std::string edge_name = (node + std::string(from[i]) + node + std::string(to[i]));
+        signs[edge_name] = sign[i];
+    }
+
+    for (int i = 0; i < names.size(); i++) {
+        std::vector<std::string> adj;
+        for (int j = 0; j < from.size(); j++) {
+            if (names[i] == from[j]) {
+                std::string nei_name = (node + to[j]);
+                adj.push_back(nei_name);
+            }
+        }
+        std::string curr = (node + names[i]);
+        neigh[curr] = adj;
+    }
+    //////////////////////////////////////////////////////////////////
+    
+    // CORE LOGIC
+    /////////////////////////////
+    int super_node = 1;
+    std::string S;
+    std::string curr;
+    while(!nias.empty()) {
+
+        S = nias.front();
+        nias.pop();
+        Rcout << supers[S] << std::endl;
+        if (supers[S] != "-1") {
+            continue;
+        }
+        
+        std::queue<std::string> Q;
+        std::map<std::string, std::string> blacklist;
+        Q.push(S);
+
+        while(!Q.empty()) {
+            curr = Q.front();
+            Q.pop();
+
+            if (blacklist[curr] != curr) {
+                colors[curr] = "2";
+                supers[curr] = std::to_string(super_node);
+                std::vector<std::string> nei = neigh[curr];
+                for (int i = 0; i < nei.size(); i++) {
+                    std::string n = nei[i];
+                    if (supers[n] == "-1") {
+                        std::string edge = (curr + n);
+                        if (signs[edge] == "1") {
+                            if (colors[n] == "1") {
+                                colors[n] = "2";
+                                Q.push(n);
+                            }
+                        }
+                        else {
+                            blacklist[n] = n;
+                            colors[n] = "1";
+                        }
+                    }
+                }
+            }
+
+        }
+        super_node = super_node + 1;
+
+    }
+
+    return supers;
+}
