49bb283c7302594b88393be291fbb03620f7a5b1
Nima Azbijari <nima@dhcp-168-105-250-42.wireless.manoa.hawaii.edu>
2019-12-12 14:24:56 -1000

    fixed return value

--- super_node_memberships.R
+++ super_node_memberships.R
@@ -7,8 +7,10 @@ super_node_memberships <- function(g) {
   V(g)$color <- 1
   V(g)$super <- -1
   V(g)$name <- V(g)$id
-  create_super_nodes(get.data.frame(g, what='vertices'), get.data.frame(g, what='edges'))
+  m <- create_super_nodes(get.data.frame(g, what='vertices'), get.data.frame(g, what='edges'))
+  strtoi(as.character(m$supers))
 }
+
 
 
 
